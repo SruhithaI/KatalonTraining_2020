9bb1444a9d58c2a1a44496e2cc647b3a92e55609
slk <slk@WSLKCMP4FT2-14.SLKSOFT.COM>
2020-01-22 10:12:36 +0530

    Day3 Training

--- Scripts/Day3_Katalon TC/Script1579668058618.groovy
+++ Scripts/Day3_Katalon TC/Script1579668058618.groovy
@@ -13,7 +13,3 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-def name = 'Automation'
-
-println('The name is ' + name)
-
